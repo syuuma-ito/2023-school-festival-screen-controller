1fe052b413f3c6ea6bda543d87eb9bdac69d0084
syuuma-ito <itosyuuma@gmail.com>
2023-09-06 01:37:14 +0900

    初期化

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# 2023-school-festival-screen-controller
+<h1 align="center">2023 クラス企画 画面制御プログラム</h1>
 
-クラス企画の画面制御プログラム
+![ロゴ](./src/img/README/logo.png)
+
+[![Open in Visual Studio Code](https://img.shields.io/static/v1?logo=visualstudiocode&label=&message=Open%20in%20Visual%20Studio%20Code&labelColor=2c2c32&color=007acc&logoColor=007acc)](https://open.vscode.dev/syuuma-ito/2023-school-festival-screen-controller)
+
+## リンク
+
+[公式サイト](https://qrtn.jp/ppq7njn "公式サイト")
--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 # インポート
+import logging
 import math
 import os
 import random
@@ -6,13 +7,12 @@ import sys
 import time
 from concurrent.futures import ThreadPoolExecutor
 
-import pygame
+import obsws_python
 import socketio
 from dotenv import load_dotenv
 from rich.traceback import install
+from websocket_server import WebsocketServer
 
-from enemy import Enemy
-from player_pointer import PlayerPointer
 from utils import logger
 from utils.clipNumber import clipNumber
 
@@ -37,27 +37,61 @@ WINDOW_WIDTH = int(os.environ.get("WINDOW_WIDTH"))
 WINDOW_HEIGHT = int(os.environ.get("WINDOW_HEIGHT"))
 
 SEVER_URL = os.environ.get("SEVER_URL")
-FPS = 60
-BACKGROUND_COLOR = (0, 0, 0)
-
-ENV = {
-    "SCENE_WIDTH_CM": SCENE_WIDTH_CM,
-    "SCENE_HEIGHT_CM": SCENE_HEIGHT_CM,
-    "RANGE_WITH_SCREEN": RANGE_WITH_SCREEN,
-    "WINDOW_WIDTH": WINDOW_WIDTH,
-    "WINDOW_HEIGHT": WINDOW_HEIGHT,
-    "SEVER_URL": SEVER_URL,
-    "FPS": FPS,
-    "BACKGROUND_COLOR": BACKGROUND_COLOR,
-}
-
 # ----------------------------------------------------------------
 # グローバル変数
 Global_X = 0
 Global_Y = 0
-Global_scene = "game"
+websocket = None
+
+USERS = {
+    "1P": {
+        "isConnect": False,
+    },
+    "2P": {
+        "isConnect": False,
+    },
+    "3P": {
+        "isConnect": False,
+    },
+}
+
+obs = obsws_python.ReqClient(
+    host=os.environ["OBSWS_HOST"],
+    port=int(os.environ["OBSWS_PORT"]),
+    password=os.environ["OBSWS_PASS"],
+    timeout=3,
+)
+
+isStart = False
+
 # ----------------------------------------------------------------
 # メイン
+
+
+class WebSocket:
+    def __init__(self, host, port):
+        self.server = WebsocketServer(host=host, port=port, loglevel=logging.DEBUG)
+        self.port = port
+        self.host = host
+        self.server.set_fn_new_client(self.new_client)
+        self.server.set_fn_message_received(self.message_received)
+        self.server.set_fn_client_left(self.client_left)
+
+    def new_client(self, client, server):
+        log.info(f"connect id: {client['id']}")
+
+    def client_left(self, client, server):
+        log.info(f"disconnect id: {client['id']}")
+
+    def message_received(self, client, server, message):
+        log.info(f"on_message {message} [id: {client['id']}]")
+
+    def run(self):
+        log.debug(f"websocket server start {self.host}:{self.port}")
+        self.server.run_forever()
+
+    def emit(self, message):
+        self.server.send_message_to_all(message)
 
 
 class IoClient:
@@ -68,6 +102,7 @@ class IoClient:
         self.socket.on("connect", self.__on_connect)
         self.socket.on("disconnect", self.__on_disconnect)
         self.socket.on("angles", self.__on_angle)
+        self.socket.on("shoot", self.__on_shoot)
 
     # イベントハンドラ
     def __on_connect(self):
@@ -78,7 +113,13 @@ class IoClient:
 
     def __on_angle(self, angle):
         global Global_X, Global_Y
+        name = angle["name"]
         Global_X, Global_Y = self.__calc_coordinate(angle["x"] - 290, angle["y"])
+        websocket.emit(f"angle,{Global_X},{Global_Y},{name}")
+
+    def __on_shoot(self, shoot):
+        print(shoot)
+        websocket.emit(f"shoot,{Global_X},{Global_Y}")
 
     def __calc_coordinate(self, theta1, theta2):
         # スマホの角度から座標を計算
@@ -100,7 +141,6 @@ class IoClient:
         y = clipNumber(round(y), -25, WINDOW_HEIGHT - 25)
         return x, y
 
-    # メイン
     def connect(self, server_url):
         log.info(f"connect to {server_url}")
         self.socket.connect(server_url)
@@ -113,78 +153,74 @@ def socket_main():
     client.connect(SEVER_URL)
 
 
-def game_main():
-    global Global_X
-    global Global_Y
+def websocket_main():
+    global websocket
+    websocket = WebSocket(host="127.0.0.1", port=13254)
+    websocket.run()
 
-    # ページの設定
-    class GamePage:
-        def __init__(self):
-            pointer_sprite = PlayerPointer((0, 0))
-            self.player_pointer = pygame.sprite.GroupSingle(pointer_sprite)
 
-            self.enemies = []
-            for _ in range(5):
-                enemy_sprite = Enemy(
-                    (
-                        random.randrange(0, WINDOW_WIDTH),
-                        random.randrange(0, WINDOW_HEIGHT),
-                    ),
-                    ENV,
-                )
-                self.enemies.append(pygame.sprite.GroupSingle(enemy_sprite))
+def set_scene_item_enabled(scene_name: str, item_name: str, enabled: bool) -> None:
+    items = obs.get_scene_item_list(scene_name).scene_items
+    for item in items:
+        if item["sourceName"] == item_name:
+            obs.set_scene_item_enabled(scene_name, item["sceneItemId"], enabled)
+            return
+    raise ValueError(f"{scene_name}に{item_name}というソースはありません")
 
-        def run(self):
-            # アップデート
-            # 描画
-            self.player_pointer.update()
-            self.player_pointer.draw(screen)
-            for enemy in self.enemies:
-                enemy.update()
-                enemy.draw(screen)
 
-    class WaitPage:
-        def __init__(self):
-            pass
+def obs_main():
+    set_scene_item_enabled("待機", "Pointer_1P", False)
+    set_scene_item_enabled("待機", "Pointer_2P", False)
+    set_scene_item_enabled("待機", "Pointer_3P", False)
+    set_scene_item_enabled("待機", "QRコード", True)
+    set_scene_item_enabled("待機", "初期設定", True)
+    set_scene_item_enabled("待機", "スタートボタン", True)
+    set_scene_item_enabled("待機", "OK", True)
 
-        def run(self):
-            # アップデート
-            # 描画
-            pass
-
-    # Pygameの初期化
-    pygame.init()
-    screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-    pygame.display.set_caption("インバーター画面プログラム")
-    clock = pygame.time.Clock()
-
-    game_page = GamePage()
-    wait_page = WaitPage()
-
-    # メインループ
+    # 始まるまで待機
     while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-
-        screen.fill(BACKGROUND_COLOR)
-        if Global_scene == "wait":
-            wait_page.run()
-        elif Global_scene == "game":
-            game_page.run()
-
-        pygame.display.flip()
-        clock.tick(FPS)
+        if isStart:
+            break
+        set_scene_item_enabled("待機", "QRコード", True)
+        set_scene_item_enabled("待機", "初期設定", True)
+        if USERS["1P"]["isConnect"]:
+            set_scene_item_enabled("待機", "Pointer_1P", True)
+        if USERS["2P"]["isConnect"]:
+            set_scene_item_enabled("待機", "Pointer_2P", True)
+        if USERS["3P"]["isConnect"]:
+            set_scene_item_enabled("待機", "Pointer_3P", True)
+        time.sleep(5)
+        set_scene_item_enabled("待機", "QRコード", False)
+        if USERS["1P"]["isConnect"]:
+            set_scene_item_enabled("待機", "Pointer_1P", True)
+        if USERS["2P"]["isConnect"]:
+            set_scene_item_enabled("待機", "Pointer_2P", True)
+        if USERS["3P"]["isConnect"]:
+            set_scene_item_enabled("待機", "Pointer_3P", True)
+        time.sleep(5)
+        set_scene_item_enabled("待機", "初期設定", False)
+        if USERS["1P"]["isConnect"]:
+            set_scene_item_enabled("待機", "Pointer_1P", True)
+        if USERS["2P"]["isConnect"]:
+            set_scene_item_enabled("待機", "Pointer_2P", True)
+        if USERS["3P"]["isConnect"]:
+            set_scene_item_enabled("待機", "Pointer_3P", True)
+        time.sleep(5)
 
 
 def main():
-    with ThreadPoolExecutor(max_workers=10) as executor:
-        executor.submit(game_main)
-        executor.submit(socket_main)
+    with ThreadPoolExecutor(max_workers=3) as executor:
+        # executor.submit(websocket_main)
+        # executor.submit(socket_main)
+        executor.submit(obs_main)
 
 
 if __name__ == "__main__":
     log.info("[緑]========= START =========[/]")
-    game_main()
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        log.debug("KeyboardInterrupt")
+    finally:
+        log.info("=== END ===")
+        sys.exit(0)
